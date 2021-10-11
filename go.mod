ccee888fdc45803aae664c69fb689509b782f48d
“zzflygo” <z573401334@gmail.com>
2021-10-11 22:05:35 +0800

    text

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/zzflygo/testmath
+
+go 1.14
---
+++ math.go
@@ -0,0 +1,8 @@
+package textmath
+
+import "fmt"
+
+func Add(n1, n2 int) int {
+	fmt.Println("gitup.com/zzflygo/textmath")
+	return n1 + n2
+}
